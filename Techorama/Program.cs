a2e8af660c212b26cde85918878ee77791d643e2
Jiri Cincura <jiricincura@microsoft.com>
2023-10-09 11:31:04 +0200

    Progress

--- Techorama/Program.cs
+++ Techorama/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace Techorama;
 
@@ -10,6 +13,9 @@ internal class Program
         {
             context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
+
+            context.Dogs.ToList();
+            //context.Set<DogOwner>().select
         }
     }
 }
@@ -17,8 +23,12 @@ internal class Program
 public class Dog
 {
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
     public Owner Owner { get; set; }
+    //public int? OwnerId { get; set; }
+    //public ICollection<Owner> Owners { get; set; }
+    //public DateOnly OwnerAssigned { get; set; }
 }
 public class Owner
 {
@@ -27,21 +37,86 @@ public class Owner
     public string LastName { get; set; }
     public ICollection<Dog> Dogs { get; set; }
 }
+//public class DogOwner
+//{
+//    public int DogId { get; set; }
+//    public int OwnerId { get; set; }
+//    public DateOnly Assigned { get; set; }
+//}
 class MyContext : DbContext
 {
-    public DbSet<Dog> Dogs { get; set; }
-    public DbSet<Owner> Owners { get; set; }
+    public DbSet<Dog> Dogs => Set<Dog>();
+    //public DbSet<Owner> Owners => Set<Owner>();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
-        optionsBuilder.UseSqlServer(@"Data Source=.\;Initial Catalog=techorama;Integrated Security=SSPI;ConnectRetryCount=0;Trust Server Certificate=True;");
-        optionsBuilder.LogTo(Console.WriteLine);
-        optionsBuilder.EnableSensitiveDataLogging();
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.\;Initial Catalog=techorama;Integrated Security=SSPI;ConnectRetryCount=0;Trust Server Certificate=True;");
+            optionsBuilder.LogTo(Console.WriteLine);
+            optionsBuilder.EnableSensitiveDataLogging();
+        }
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
+
+        foreach (var entity in modelBuilder.Model.GetEntityTypes())
+        {
+            foreach (var prop in entity.GetProperties())
+            {
+                if (prop.ClrType == typeof(string))
+                {
+                    prop.SetMaxLength(50);
+                }
+            }
+        }
+
+        //modelBuilder.Entity<Dog>(b =>
+        //{
+        //});
+
+        modelBuilder.Entity<Owner>(builder =>
+        {
+            builder.Property(x => x.Id)
+                .UseHiLo();
+        });
+
+        modelBuilder.ApplyConfiguration(new DogConfiguration());
     }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        base.ConfigureConventions(configurationBuilder);
+
+        //configurationBuilder.Conventions.(x => cndsjkasd)
+    }
+}
+
+class DogConfiguration : IEntityTypeConfiguration<Dog>
+{
+    public void Configure(EntityTypeBuilder<Dog> builder)
+    {
+        builder.ToTable("T_Dogs");
+        builder.HasKey(x => x.Id);
+        builder.HasAlternateKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .UseHiLo();
+        builder.Property(x => x.Name)
+            .HasMaxLength(50)
+            .IsRequired()
+            .IsFixedLength(false)
+            .IsUnicode()
+            .HasColumnName("Name");
+        //builder.HasMany(x => x.Owners)
+        //    .WithMany(x => x.Dogs)
+        //    .UsingEntity<DogOwner>();
+    }
+}
+
+class StringConvention : IConvention
+{
+
 }
